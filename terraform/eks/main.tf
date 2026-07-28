justadevopsguy/t1-aws-eks-webapp

feat: add infra related files

terraform/eks/main.tf
@@ -1,0 +1,38 @@
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "t1-terraform-bucket"
+    key    = "aws/terraform/vpc/telite-vpc.tfstate"
+    region = "ap-southeast-2"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name    = var.cluster_name
+  kubernetes_version = var.cluster_version
+
+  vpc_id                   = data.terraform_remote_state.vpc.outputs.vpc_id
+  subnet_ids               = data.terraform_remote_state.vpc.outputs.private_subnets
+  control_plane_subnet_ids = data.terraform_remote_state.vpc.outputs.public_subnets
+
+  endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
+  
+  eks_managed_node_groups = {
+    main = {
+      name = "${var.cluster_name}-node-group"
+      instance_types = ["t3.micro"]
+      capacity_type  = "ON_DEMAND"
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
+      ami_type     = "AL2023_x86_64_STANDARD"
+      disk_size    = 20
+      tags = var.common_tags
+    }
+  }
+  tags = var.common_tags
+}

terraform/eks/providers.tf
@@ -1,0 +1,21 @@
+terraform {
+  required_version = ">= 1.12.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "t1-terraform-bucket"
+    key            = "aws/terraform/eks/telite-eks.tfstate"
+    region         = "ap-southeast-2"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}

terraform/eks/variables.tf
@@ -1,0 +1,26 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-southeast-2"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+  default     = "telite-eks"
+}
+
+variable "cluster_version" {
+  description = "EKS Kubernetes version"
+  type        = string
+  default     = "1.33"
+}
+
+variable "common_tags" {
+  description = "Common tags for resources"
+  type        = map(string)
+  default     = {
+    Environment = "production"
+    Project     = "telite"
+  }
+}

terraform/vpc/providers.tf
@@ -1,0 +1,21 @@
+terraform {
+  required_version = ">= 1.12.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "t1-terraform-bucket"
+    key            = "aws/terraform/vpc/telite-vpc.tfstate"
+    region         = "ap-southeast-2"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}

terraform/vpc/variables.tf
@@ -1,0 +1,44 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-southeast-2"
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "telite-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.10.0.0/20"
+}
+
+variable "private_subnets" {
+  description = "Private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.10.0.0/24", "10.10.1.0/24", "10.10.2.0/24"]
+}
+
+variable "public_subnets" {
+  description = "Public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.10.3.0/24", "10.10.4.0/24", "10.10.5.0/24"]
+}
+
+variable "database_subnets" {
+  description = "Database subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.10.6.0/24", "10.10.7.0/24", "10.10.8.0/24"]
+}
+
+variable "common_tags" {
+  description = "Common tags for resources"
+  type        = map(string)
+  default     = {
+    Environment = "production"
+    Project     = "telite"
+  }
+}

justadevopsguy/aws-eks-webapp

terraform/eks/main.tf
@@ -1,0 +1,38 @@
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "t1-terraform-bucket"
+    key    = "aws/terraform/vpc/ajq-vpc.tfstate"
+    region = "ap-southeast-2"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name    = var.cluster_name
+  kubernetes_version = var.cluster_version
+
+  vpc_id                   = data.terraform_remote_state.vpc.outputs.vpc_id
+  subnet_ids               = data.terraform_remote_state.vpc.outputs.private_subnets
+  control_plane_subnet_ids = data.terraform_remote_state.vpc.outputs.public_subnets
+
+  endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
+  
+  eks_managed_node_groups = {
+    main = {
+      name = "${var.cluster_name}-node-group"
+      instance_types = ["t3.micro"]
+      capacity_type  = "ON_DEMAND"
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
+      ami_type     = "AL2023_x86_64_STANDARD"
+      disk_size    = 20
+      tags = var.common_tags
+    }
+  }
+  tags = var.common_tags
+}

terraform/eks/providers.tf
@@ -1,0 +1,21 @@
+terraform {
+  required_version = ">= 1.12.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "t1-terraform-bucket"
+    key            = "aws/terraform/eks/ajq-eks.tfstate"
+    region         = "ap-southeast-2"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}

terraform/eks/variables.tf
@@ -1,0 +1,26 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-southeast-2"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+  default     = "ajq-eks"
+}
+
+variable "cluster_version" {
+  description = "EKS Kubernetes version"
+  type        = string
+  default     = "1.33"
+}
+
+variable "common_tags" {
+  description = "Common tags for resources"
+  type        = map(string)
+  default     = {
+    Environment = "production"
+    Project     = "ajq"
+  }
+}

terraform/vpc/providers.tf
@@ -1,0 +1,21 @@
+terraform {
+  required_version = ">= 1.12.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "t1-terraform-bucket"
+    key            = "aws/terraform/vpc/ajq-vpc.tfstate"
+    region         = "ap-southeast-2"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}

terraform/vpc/variables.tf
@@ -1,0 +1,44 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-southeast-2"
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "ajq-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.10.0.0/20"
+}
+
+variable "private_subnets" {
+  description = "Private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.10.0.0/24", "10.10.1.0/24", "10.10.2.0/24"]
+}
+
+variable "public_subnets" {
+  description = "Public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.10.3.0/24", "10.10.4.0/24", "10.10.5.0/24"]
+}
+
+variable "database_subnets" {
+  description = "Database subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.10.6.0/24", "10.10.7.0/24", "10.10.8.0/24"]
+}
+
+variable "common_tags" {
+  description = "Common tags for resources"
+  type        = map(string)
+  default     = {
+    Environment = "production"
+    Project     = "ajq"
+  }
+}